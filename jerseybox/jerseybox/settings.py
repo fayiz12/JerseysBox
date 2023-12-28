e3f83426f390b0ae2133dec44b25784f49feb19e
muhammed fayiz <muhammedfayiz@muhammeds-MacBook-Air-2.local>
2023-12-28 15:51:20 +0530

    new commit

--- jerseybox/jerseybox/settings.py
+++ jerseybox/jerseybox/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-AALLOWED_HOSTS = ["65.1.220.142","localhost","www.jerseysbox.shop"]
+ALLOWED_HOSTS = ["65.1.220.142","localhost","www.jerseysbox.shop"]
 
 
 # Application definition
